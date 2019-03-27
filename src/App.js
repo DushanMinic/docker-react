011aa97374487ed910509fa0c5e3f24d210fe803
Dušan Minić <dushanminic@gmail.com>
2019-03-27 21:36:42 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Docker test.</p>
+          <p>Docker test. I got changed</p>
           <a
             className="App-link"
             href="https://reactjs.org"
